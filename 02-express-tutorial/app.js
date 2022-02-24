ddd27149e7c1091be2bdae88bc2343766d7e702b
ukasz1 <lukaszmitkowski53@gmail.com>
2022-02-24 15:25:20 +0100

    http server works!

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -17,13 +17,32 @@ const server = http.createServer((req, res) => {
     res.write(homePage)
     res.end();
   }
-  //about about
+  // about about
   else if (url === '/about') {
     res.writeHead(200, { 'content-type': 'text/html' })
     res.write('<h1>about page</h1>')
     res.end();
-
   }
+  // styles
+  else if (url === '/styles.css') {
+    res.writeHead(200, { 'content-type': 'text/css' })
+    res.write(homeStyles)
+    res.end();
+  }
+  // image/logo
+  else if (url === '/logo.svg') {
+    res.writeHead(200, { 'content-type': 'image/svg+xml' })
+    res.write(homeImage)
+    res.end();
+  }
+  // logic
+  else if (url === '/browser-app.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' })
+    res.write(homeLogic)
+    res.end();
+  }
+
+
   // 404
   else {
     res.writeHead(404, { 'content-type': 'text/html' })
