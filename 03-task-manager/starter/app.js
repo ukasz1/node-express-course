9bc94c89561c1aae620a02ee79d51092213732b6
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-02 15:49:45 +0100

    connection to mongoDB, schema

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
+require('dotenv').config();
 
+const connectDB = require('./db/connect');
 
 // middleware
 app.use(express.json())
@@ -14,10 +16,18 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks);
 
-
-
-
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`);
-})
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}...`);
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start();
