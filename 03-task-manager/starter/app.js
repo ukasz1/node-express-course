543073ceff9c22cfad60105e68a1afdbf0c2e68f
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-03 13:36:48 +0100

    error handler middleware

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -10,11 +10,13 @@ app.use(express.static('./public'))
 app.use(express.json())
 
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // routes
 
 app.use('/api/v1/tasks', tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
