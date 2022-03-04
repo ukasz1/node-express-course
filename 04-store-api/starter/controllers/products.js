f40d32f38c244a4e527f523eb6c42237f22ea435
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-04 10:02:58 +0100

    04 first queries

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,21 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-  //throw new Error('testing async errors');
-  res.status(200).json({ msg: 'products testing route' })
+  const products = await Product.find({
+    name: 'vase table'
+  })
+  res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' })
+  const { featured } = req.query;
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
