3cd5cae311fd52da461f2811241c0391ef1f2b28
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-04 12:03:51 +0100

    04 sort queries

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,20 +1,35 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: 'vase table'
-  })
+  const search = 'ab';
+  const products = await Product.find({}).sort('name');
   res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {}
 
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+    console.log(sort)
+  }
+  else { // default sorting by date
+    result = result.sort('createAt');
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length })
 }
 
