f2b0d1578ddeb9f005016c5c38c670712eaaf250
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-08 12:03:40 +0100

    06 schema fixed, register request works

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -16,7 +16,7 @@ const UserSchema = new mongoose.Schema({
     ],
     unique: true
   },
-  name: {
+  password: {
     type: String,
     required: [true, 'Please provice password'],
     minlength: 6,
