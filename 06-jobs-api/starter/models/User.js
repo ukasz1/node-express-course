3d2e4784c4077b9e470df35579da5b64f2b1fd45
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-08 16:17:20 +0100

    06 login cpl

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -30,5 +31,14 @@ UserSchema.pre('save', async function (next) {
 
   next();
 })
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME })
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch;
+}
 
 module.exports = mongoose.model('user', UserSchema);
