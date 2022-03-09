41263b2a99e5cd28c637961e9f73673ce9717d27
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-09 15:41:05 +0100

    06 update and delete cpl, complete functionality

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -7,7 +7,16 @@ const getAllJobs = async (req, res) => {
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 }
 const getJob = async (req, res) => {
-  res.send('get job')
+  const { user: { userId }, params: { id: jobId } } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId
+  })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
@@ -15,10 +24,35 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job })
 }
 const updateJob = async (req, res) => {
-  res.send('update job')
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId }
+  } = req;
+
+  if (company === '' || position === '') {
+    throw new BaadRequestError('Company or Position fields cannot be empty')
+  }
+  const job = await Job.findByIdAndUpdate({ _id: jobId, createdBy: userId }, req.body, { new: true, runValidators: true })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
+
 }
 const deleteJob = async (req, res) => {
-  res.send('delete job')
+  const { user: { userId }, params: { id: jobId } } = req;
+
+  const job = await Job.findByIdAndRemove({
+    _id: jobId,
+    createdBy: userId
+  })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).send()
 }
 
 
