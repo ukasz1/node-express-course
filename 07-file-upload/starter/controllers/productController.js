fad8cf7377ccb2e2745736cfe21e99a5df19cf68
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-10 16:16:44 +0100

    07 wrong controller fixed

--- 07-file-upload/starter/controllers/productController.js
+++ 07-file-upload/starter/controllers/productController.js
@@ -1,18 +1,10 @@
 const Product = require('../models/Product')
 
-const path = require('path')
+
 const { StatusCodes } = require('http-status-codes')
 
 const createProduct = async (req, res) => {
-  const productImage = req.files.image;
-
-  const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
-
-  await productImage.mv(imagePath);
-
-  return res
-    .status(StatusCodes.OK)
-    .json({ image: { src: `/uploads/${productImage.name}` } })
+  res.send('create product')
 
 }
 const getAllProducts = async (req, res) => {
--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,7 +1,18 @@
 const { StatusCodes } = require('http-status-codes')
 
+const path = require('path')
+
 const uploadProductImage = async (req, res) => {
-  res.send('upload product image')
+  const productImage = req.files.image;
+
+  const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
+
+  await productImage.mv(imagePath);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } })
+
 }
 
 module.exports = {
