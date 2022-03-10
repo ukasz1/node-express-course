c181e7dce1d0c9925f6181cb57052bff5b93567b
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-10 12:52:01 +0100

    06 COMPLETE PROJECT - documentation added

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -9,6 +9,12 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
 // connectDB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
@@ -33,7 +39,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// extra packages
+app.get('/', (req, res) => {
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter);
