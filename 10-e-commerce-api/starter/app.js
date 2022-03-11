76b2d4c93ff5458c56dcf6deaeac3f86fd107d86
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-11 13:11:29 +0100

    10 express, db and middleware set

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,43 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+require('express-async-errors')
+
+// express
+const express = require('express');
+const app = express();
+
+// rest of the packages
+const morgan = require('morgan')
+
+// datatbase
+const connectDB = require('./db/connect')
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(morgan('tiny'))
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+  res.send('e-commerce API')
+})
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server listening on a port ${port}...`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start();
