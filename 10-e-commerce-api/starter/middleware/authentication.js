d15a279cf0acbe068451c093d7722fdebc3aa410
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-14 15:50:53 +0100

    10 admin authorizing cpl

--- 10-e-commerce-api/starter/middleware/authentication.js
+++ 10-e-commerce-api/starter/middleware/authentication.js
@@ -14,9 +14,18 @@ const authenticateUser = async (req, res, next) => {
   } catch (error) {
     throw new CustomError.UnauthenticatedError('Authentication Invalid')
   }
+}
 
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError('Unauthorized to access this route')
+    }
+    next();
+  }
 }
 
 module.exports = {
-  authenticateUser
+  authenticateUser,
+  authorizePermissions
 }
--- 10-e-commerce-api/starter/routes/userRoutes.js
+++ 10-e-commerce-api/starter/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticateUser } = require('../middleware/authentication')
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication')
 
 const {
   getAllUsers,
@@ -10,9 +10,9 @@ const {
   updateUserPassword
 } = require('../controllers/userController');
 
-router.route('/').get(authenticateUser, getAllUsers);
+router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllUsers);
 
-router.route('/showMe').get(showCurrentUser)
+router.route('/showMe').get(authenticateUser, showCurrentUser)
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
 
