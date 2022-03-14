b42dda89acbb167e7fd48112583f4f058a34dd29
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-14 16:05:22 +0100

    10 showCurrentUser cpl

--- 10-e-commerce-api/starter/middleware/authentication.js
+++ 10-e-commerce-api/starter/middleware/authentication.js
@@ -5,14 +5,14 @@ const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
 
   if (!token) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid')
+    throw new CustomError.UnauthenticatedError('Authentication Invalid. No token')
   }
   try {
     const { name, userId, role } = isTokenValid({ token });
     req.user = { name, userId, role }
     next();
   } catch (error) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid')
+    throw new CustomError.UnauthenticatedError('Authentication Invalid. Token Invalid')
   }
 }
 
