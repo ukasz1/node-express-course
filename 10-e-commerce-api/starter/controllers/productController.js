3778fb3a93b2db40a0d2ec000ae20223f2a818dd
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-15 12:48:03 +0100

    10 product controler (no image upload)

--- 10-e-commerce-api/starter/controllers/productController.js
+++ 10-e-commerce-api/starter/controllers/productController.js
@@ -10,17 +10,43 @@ const createProduct = async (req, res) => {
   res.send('create product');
 };
 const getAllProducts = async (req, res) => {
-  res.send('get all products');
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products, count: products.length })
 };
+
 const getSingleProduct = async (req, res) => {
-  res.send('get single product');
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`)
+  }
+  res.status(StatusCodes.OK).json({ product })
 };
+
 const updateProduct = async (req, res) => {
-  res.send('update product');
+  const { id: productId } = req.params;
+  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+    new: true,
+    runValidators: true
+  })
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`)
+  }
+
+  res.status(StatusCodes.OK).json({ product })
 };
+
 const deleteProduct = async (req, res) => {
-  res.send('delete product');
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`)
+  }
+  await product.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Product removed' })
 };
+
 const uploadImage = async (req, res) => {
   res.send('upload image');
 };
