72fc47f7a522d3777b4c5d91bb87b047eca42f61
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-15 11:36:40 +0100

    10 product routing cpl

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -15,6 +15,7 @@ const connectDB = require('./db/connect')
 // routers
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
 
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
@@ -37,6 +38,7 @@ app.get('/api/v1', (req, res) => {
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/products', productRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
