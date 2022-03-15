b1124a4ed78969f3aa7bb0213ad51f171a3de429
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-15 16:14:01 +0100

    10 review controller completed

--- 10-e-commerce-api/starter/controllers/reviewController.js
+++ 10-e-commerce-api/starter/controllers/reviewController.js
@@ -1,4 +1,31 @@
+const Review = require('../models/Review')
+const Product = require('../models/Product')
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { checkPermissions } = require('../utils')
+
 const createReview = async (req, res) => {
+  const { product: productId } = req.body;
+
+  const isValidProduct = await Product.findOne({ _id: productId })
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId
+  })
+
+  if (alreadySubmitted) {
+    throw new CustomError.BadRequestError('Already submitted for this product')
+  }
+
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body)
+  res.status(StatusCodes.CREATED).json({ review });
+
+
   res.send('create review');
 };
 const getAllReviews = async (req, res) => {
