4d3aa3c9abe035238c23394c13328679ddc4a450
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-15 16:57:14 +0100

    10 review controller (TRULY) complete

--- 10-e-commerce-api/starter/controllers/reviewController.js
+++ 10-e-commerce-api/starter/controllers/reviewController.js
@@ -24,21 +24,57 @@ const createReview = async (req, res) => {
   req.body.user = req.user.userId;
   const review = await Review.create(req.body)
   res.status(StatusCodes.CREATED).json({ review });
-
-
-  res.send('create review');
 };
+//-------------------------------------------------------
 const getAllReviews = async (req, res) => {
-  res.send('get all reviews');
+  const reviews = await Review.find({});
+  res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
 };
+//-------------------------------------------------------
 const getSingleReview = async (req, res) => {
-  res.send('get single review');
+  const { id: reviewId } = req.params;
+
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id: ${reviewId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ review });
 };
+//-------------------------------------------------------
 const updateReview = async (req, res) => {
-  res.send('update review');
+  const { id: reviewId } = req.params;
+  const { rating, title, comment } = req.body;
+
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id: ${reviewId}`);
+  }
+
+  checkPermissions(req.user, review.user);
+
+  review.rating = rating;
+  review.title = title;
+  review.comment = comment;
+
+  await review.save();
+  res.status(StatusCodes.OK).json({ review })
+
 };
+
+//-------------------------------------------------------
 const deleteReview = async (req, res) => {
-  res.send('delete review');
+  const { id: reviewId } = req.params;
+
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id: ${reviewId}`);
+  }
+
+  checkPermissions(req.user, review.user);
+  await review.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Review removed' })
+
 };
 
 module.exports = {
