dc0d01b968f58d90174e54781e2150d659fc78cf
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-17 14:16:39 +0100

    11 nodemailer config in a separate file

--- 11-auth-workflow/starter/server/utils/sendEmail.js
+++ 11-auth-workflow/starter/server/utils/sendEmail.js
@@ -1,23 +1,16 @@
 const nodemailer = require('nodemailer');
+const nodemailerConfig = require('./nodemailerConfig')
 
-const sendEmail = async () => {
+const sendEmail = async ({ to, subject, html }) => {
   let testAccount = await nodemailer.createTestAccount();
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-      user: 'rebeka.eichmann62@ethereal.email',
-      pass: 'xpKPGqRKsPzSM7w4JA'
-    }
-  });
+  const transporter = nodemailer.createTransport(nodemailerConfig);
 
-  let info = await transporter.sendMail({
+  return transporter.sendMail({
     from: '"Coding Addict 👻" <itaf2@interia.pl>', // sender address
-    to: "user@user.com", // list of receivers
-    subject: "Testing Email", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Testing Email</b>", // html body
+    to,
+    subject,
+    html
   });
 }
 
