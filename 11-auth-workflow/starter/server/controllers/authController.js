685405aa151461773f20de9923da78cb244389af
ukasz1 <lukaszmitkowski53@gmail.com>
2022-03-21 11:30:02 +0100

    11 logout functionality

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -114,9 +114,16 @@ const login = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
-  res.cookie('token', 'logout', {
+
+  await Token.findOneAndDelete({ user: req.user.userId })
+
+  res.cookie('accessToken', 'logout', {
     httpOnly: true,
-    expires: new Date(Date.now() + 1000),
+    expires: new Date(Date.now()),
+  });
+  res.cookie('refreshToken', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
   });
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
